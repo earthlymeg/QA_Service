1d4667c6117f71d85a5827b7575f138304869c59
Tyler Duncan <tylerduncan365@yahoo.com>
2020-11-13 18:25:38 -0600

    Created basic mock up of AddQuestion component using react-hook-form

--- client/src/components/AddQuestion.jsx
+++ client/src/components/AddQuestion.jsx
@@ -1,3 +1,24 @@
 /*
-Class component to house the form, get user input and then call the function for routing a question to the api.
+component to house the form, get user input and then call the function for routing a question to the api.
 */
+
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const AddQuestion = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+
+      <input name='name' defaultValue='Name' ref={register({ required: true })} />
+      <input name='body' defaultValue='Ask a Question' ref={register({ required: true })} />
+      <input name='email' defaultValue='Email' ref={register({ required: false })} />
+
+      <input type="submit" />
+    </form>
+  )
+};
+
+export default AddQuestion;
