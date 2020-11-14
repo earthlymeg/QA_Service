c1f7dce56306dffc599fa8407efad9843ef1c6d6
Tyler Duncan <tylerduncan365@yahoo.com>
2020-11-13 18:35:14 -0600

    Created search bar component and currently logs data to console

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -12,3 +12,22 @@ Class component:
   onSubmit - to call a function from the QA_Module with the input of the search field as it's argument, changing a filter property
 
 */
+
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const SearchBar = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+
+      <input name='filter' defaultValue='Have a question? Search for answers...' ref={ register }/>
+
+      <input type="submit" />
+    </form>
+  )
+};
+
+export default SearchBar;
