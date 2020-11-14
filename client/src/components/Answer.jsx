6c91eabe3fe1d0b2bb0371e2022bbe1ef623d142
Tyler Duncan <tylerduncan365@yahoo.com>
2020-11-13 18:27:17 -0600

    Created basic mock up of Answer component using dummy data

--- client/src/components/Answer.jsx
+++ client/src/components/Answer.jsx
@@ -5,3 +5,32 @@ Functional component to be rendered within the Question Component.
   Ability to display pictures
 
 */
+import React from 'react';
+import Helpful from './Helpful.jsx';
+import AnswerPhoto from './AnswerPhoto.jsx';
+
+const Answer = (props) => {
+  console.log('props in Answer: ', props)
+  return (
+    <div>
+      <p><span>A:</span> {props.answer.body}</p>
+      <Helpful helpfulness={props.answer.helpfulness} />
+      <div>
+        {props.answer.photos.map((photo) => {
+          return (
+            <span key={photo}>
+              <AnswerPhoto photoSrc={photo} />
+            </span>
+          )
+        })}
+      </div>
+
+
+      <p>by {props.answer.answerer_name}</p>
+      {/* conditionally render the click button, only display if more answers are available */}
+      <button>Load More Answers</button>
+    </div>
+  )
+};
+
+export default Answer;
